e0b21a60f06291ca9c30aa4f8e4a3a6240875561
J-CamiloG <jc6381878@gmail.com>
2024-06-18 21:47:32 -0500

    inicilizacion del proyecto

---
+++ main.js
@@ -0,0 +1,9 @@
+const typed = new Typed('.typed',{
+    strings: ["Full Stack", "Front End", "Back End"],
+
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    loop:true,
+    cursorChar: '|',
+})
